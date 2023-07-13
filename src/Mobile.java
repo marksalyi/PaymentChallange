d7aa206f318177d829320c1fa98c460f2f9e57fb
marksalyi <salyi.mark@gmail.com>
2023-07-13 16:14:49 +0200

    refreshed version

--- src/Mobile.java
+++
@@ -1,8 +0,0 @@
-public abstract class Mobile {
-    void call(){
-        System.out.println("Calling: ");
-    };
-    void sendmessage(){
-        System.out.println("Sending message: ");
-    };
-}
